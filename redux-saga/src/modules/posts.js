f243eb6b7250aaa806c0b410ecdb9b50d78314d4
HaJunRyu <hjr4557@naver.com>
2021-07-22 17:46:27 +0900

    redux-saga로 Promise 사용 및 history 사용
    Changes to be committed:
    modified:   src/index.js
    modified:   src/modules/index.js
    modified:   src/modules/posts.js

--- redux-saga/src/modules/posts.js
+++ redux-saga/src/modules/posts.js
@@ -1,5 +1,6 @@
 import * as postsAPI from '../api/posts';
 import { reducerUtils } from '../lib/asyncUtils';
+import { call, put, takeEvery, getContext } from 'redux-saga/effects';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -9,67 +10,46 @@ const GET_POST_BY_ID = 'GET_POST_BY_ID';
 const GET_POST_BY_ID_SUCCESS = 'GET_POST_BY_ID_SUCCESS';
 const GET_POST_BY_ID_ERROR = 'GET_POST_BY_ID_ERROR';
 
-export const getPosts = () => async dispatch => {
-  dispatch({ type: GET_POSTS });
-  try {
-    const posts = await postsAPI.getPosts();
-    dispatch({ type: GET_POSTS_SUCCESS, payload: { posts } });
-  } catch (error) {
-    dispatch({ type: GET_POSTS_ERROR, payload: { error } });
-  }
-};
+const GO_TO_HOME = 'GO_TO_HOME';
 
-export const getPostById = id => async dispatch => {
-  dispatch({ type: GET_POST_BY_ID, meta: id });
-  try {
-    const post = await postsAPI.getPostById(id);
-    dispatch({
-      type: GET_POST_BY_ID_SUCCESS,
-      payload: { post },
-      meta: id,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_POST_BY_ID_ERROR,
-      payload: { error },
-      error: true,
-      meta: id,
-    });
-  }
-};
+export const getPosts = () => ({ type: GET_POSTS });
+export const getPostById = id => ({ type: GET_POST_BY_ID, payload: { id }, meta: id });
 
-export const goToHome =
-  () =>
-  (dispatch, getState, { history }) => {
-    history.push('/');
-  };
+export const goToHome = () => ({ type: GO_TO_HOME });
+
+function* getPostsSaga() {
+  try {
+    const posts = yield call(postsAPI.getPosts);
+    yield put({ type: GET_POSTS_SUCCESS, payload: { posts } });
+  } catch (error) {
+    yield put({ type: GET_POSTS_ERROR, payload: { error }, error: true });
+  }
+}
+
+function* getPostByIdSaga(action) {
+  const { id } = action.payload;
+  try {
+    const post = yield call(postsAPI.getPostById, id);
+    yield put({ type: GET_POST_BY_ID_SUCCESS, payload: { post }, meta: id });
+  } catch (error) {
+    yield put({ type: GET_POST_BY_ID_ERROR, payload: { error }, error: true, meta: id });
+  }
+}
+
+function* goToHomeSaga() {
+  const history = yield getContext('history');
+  history.push('/');
+}
+
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST_BY_ID, getPostByIdSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
+}
 
 const initialState = {
   posts: reducerUtils.initial(),
   post: {},
-};
-
-const getPostReducer = (state, action) => {
-  const id = action.meta;
-  switch (action.type) {
-    case GET_POST_BY_ID:
-      return {
-        ...state,
-        post: { ...state.post, [id]: reducerUtils.loading(state.post[id]?.data) },
-      };
-    case GET_POST_BY_ID_SUCCESS:
-      return {
-        ...state,
-        post: { ...state.post, [id]: reducerUtils.success(action.payload.post) },
-      };
-    case GET_POST_BY_ID_ERROR:
-      return {
-        ...state,
-        post: { ...state.post, [id]: reducerUtils.error(action.payload.error) },
-      };
-    default:
-      return state;
-  }
 };
 
 export default function postsReducer(state = initialState, action) {
